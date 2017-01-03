a2a32da8196d093cfb9d84bd7ff046ef715b377a
Vishesh Thakur <visheshsingh16@gmail.com>
2017-01-02 22:31:08 -0500

    Initial commit

---
+++ mainscript.js
@@ -0,0 +1,10 @@
+console.log($("h2").attr("reply"));
+
+var Person = function (fname, lname) {
+    this.fname = fname;
+    this.lname = lname;
+    
+};
+
+var vishesh = new Person("Vishesh", "thakur");
+console.log(vishesh);
